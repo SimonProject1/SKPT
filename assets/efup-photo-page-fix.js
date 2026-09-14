8189e70af363a48d1ded6877c23db74042498be4
SimonProject1 <simon.kiesler@bayer.com>
2026-09-14 12:58:09 +0200

    Add files via upload

--- assets/efup-photo-page-fix.js
+++ assets/efup-photo-page-fix.js
@@ -1,2 +1,2 @@
-/* Bayer PLT Tools V1.0.1 - Marker fuer den aktiven PDF-Fotoseiten-Fix. */
-window.BAYER_PLT_PHOTO_PAGE_FIX = '1.0.1';
+/* Bayer PLT Tools V1.2.0 - Marker fuer den aktiven PDF-Fotoseiten-Fix. */
+window.BAYER_PLT_PHOTO_PAGE_FIX = '1.2.0';
