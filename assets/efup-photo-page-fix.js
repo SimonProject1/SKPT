d5d64ad586fbc0b77e4f2affb940b4d36949da29
SimonProject1 <simon.kiesler@bayer.com>
2026-09-14 11:57:17 +0200

    Add files via upload

---
+++ assets/efup-photo-page-fix.js
@@ -0,0 +1,5 @@
+/* Bayer PLT Tools V1.0.1
+   Markerdatei fuer den PDF-Fotoseiten-Fix.
+   Die eigentliche Korrektur wird beim Laden der Messstellen-Dokumentation
+   durch den Service Worker in die vorhandene PDF-Logik eingesetzt. */
+window.BAYER_PLT_PHOTO_PAGE_FIX = '1.0.1';
