9e90f0139d128e37bbe0dc7591d57cd8cfecb220
SimonProject1 <simon.kiesler@bayer.com>
2026-09-25 17:34:48 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,29 @@
-const CACHE='sk-plt-tools-v1.4.0';const FILES=['./','./index.html','./manifest.webmanifest','./assets/styles.css','./assets/app.js','./assets/logo.png','./assets/icon-192.png','./assets/icon-512.png','./assets/apple-touch-icon.png','./analogsignal/','./pf-rechner/','./pt-rechner/','./einheitenrechner/','./messstellen-doku/','./servicewerte/','./wissensdatenbank/'];self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request))))
+const CACHE='sk-plt-tools-v1.4.0-logo-final';
+const FILES=[
+  './','./index.html','./manifest.webmanifest',
+  './assets/styles.css','./assets/logo-layout.css','./assets/app.js',
+  './assets/logo.png','./assets/logo-horizontal.png',
+  './assets/icon-192.png','./assets/icon-512.png',
+  './assets/apple-touch-icon.png','./assets/favicon.png',
+  './analogsignal/','./pf-rechner/','./pt-rechner/',
+  './einheitenrechner/','./messstellen-doku/',
+  './servicewerte/','./wissensdatenbank/'
+];
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys()
+    .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+    .then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',event=>{
+  event.respondWith(fetch(event.request,{cache:'no-store'})
+    .then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+      return response;
+    })
+    .catch(()=>caches.match(event.request)));
+});
